00fbb6da2b88e69f1bd9c17f36a32fcde43df254
mikdaad <mikxplode@gmail.com>
2025-02-05 11:48:20 +0530

    fifth commit

--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -18,21 +18,21 @@ export default function IndexPage() {
 
 
 
-<h1 className="absolute text-white font-glancyr font-medium text-4xl lg:text-3xl mt-32 sm:left-10 lg:left-60">
+<h1 className="absolute text-white font-glancyr font-medium text-4xl lg:text-[1.875rem] mt-32 sm:left-10 lg:left-60">
   W E L C O M E &nbsp; T O &nbsp; T E R R I F I C
 </h1>
 
-<h1 className="absolute text-white font-glancyr font-large text-4xl lg:text-7xl mt-48 left-60">
+<h1 className="absolute text-white font-glancyr font-large text-4xl lg:text-[4.5rem] mt-48 left-60">
   WHERE MODERN ELEGANCE
 </h1>
 
-<h1 className="absolute text-white font-glancyr font-medium text-7xl lg:text-7xl mt-72 left-60">
+<h1 className="absolute text-white font-glancyr font-medium text-7xl lg:text-[4.5rem] mt-72 left-60">
   MEETS TIMELESS 
-  <span className="text-[#EED359] font-glancyr font-medium text-7xl"> STYLE</span>
+  <span className="text-[#EED359] font-glancyr font-medium text-[4.5rem]"> STYLE</span>
 </h1>
 
 
-<h1 className="absolute text-white font-glancyr text-4xl lg:text-xl mt-96 left-60 max-w-[900px] leading-relaxed">
+<h1 className="absolute text-white font-glancyr text-[1.25rem] lg:text-[1.25rem] mt-96 left-60 max-w-[900px] leading-relaxed">
   Discover fashion that speaks to your unique sense of self, with designs that redefine what it means to be 
   timelessly chic. Step into a world of confidence, innovation, and unparalleled style with <span className="font-bold">TERRIFIC</span>.
 </h1>
