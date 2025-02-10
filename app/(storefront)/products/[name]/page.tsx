c6fafabe95784db7e2b62c3360ebfb8e7a7f34e5
mikdaad <mikxplode@gmail.com>
2025-02-10 15:26:06 +0530

    product card progress - 4

--- app/(storefront)/products/[name]/page.tsx
+++ app/(storefront)/products/[name]/page.tsx
@@ -104,7 +104,7 @@ export default async function CategoriesPage({
       <h1 className="font-semibold text-3xl my-5">{title}</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
         {data.map((item) => (
-          <ProductList/>
+          <ProductList status="Dealoftheday"/>
         ))}
       </div>
     </section>
